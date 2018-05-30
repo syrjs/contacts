b17de1cfb1baa59aa8576b89451b93992bc6e5a4
Derek Anderson <dereanderson@paypal.com>
2018-05-30 11:52:05 -0700

    commiting the base files for syr contactS

---
+++ ios/SyrContacts/SyrContacts.h
@@ -0,0 +1,13 @@
+//
+//  SyrContacts.h
+//  SyrContacts
+//
+//  Created by Anderson,Derek on 5/30/18.
+//  Copyright © 2018 syr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SyrContacts : NSObject
+
+@end
---
+++ ios/SyrContacts/SyrContacts.m
@@ -0,0 +1,13 @@
+//
+//  SyrContacts.m
+//  SyrContacts
+//
+//  Created by Anderson,Derek on 5/30/18.
+//  Copyright © 2018 syr. All rights reserved.
+//
+
+#import "SyrContacts.h"
+
+@implementation SyrContacts
+
+@end
